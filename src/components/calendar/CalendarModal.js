0a8d370019c03fd7db77c0205882420cf20ff172
Manuel Blanco <manuel.blanco77@gmail.com>
2020-10-05 15:48:17 -0300

    Agregando un nuevo evento en el calendario y mostrandolo en el estado

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -7,6 +7,7 @@ import moment from 'moment';
 import Swal from 'sweetalert2';
 
 import { uiCloseModal } from '../../actions/ui';
+import { eventAddNew } from '../../actions/events';
  
 const customStyles = {
   content : {
@@ -82,6 +83,15 @@ export const CalendarModal = () => {
         if ( title.trim().length < 2 ) {
             return setTitleValid(false);
         }
+        
+        dispatch( eventAddNew({
+            ...formValues,
+            id: new Date().getTime(),
+            user: {
+                _id: '123',
+                name: 'Manu'
+            }
+        }) );
 
         setTitleValid(true);
         closeModal();
