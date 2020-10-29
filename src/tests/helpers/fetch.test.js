458cee01179a164b0510c219cb0cd5b7997a49d0
Manuel Blanco <manuel.blanco77@gmail.com>
2020-10-29 09:30:34 -0300

    Pruebas en helpers/fetch => fetchConToken

--- src/tests/helpers/fetch.test.js
+++ src/tests/helpers/fetch.test.js
@@ -1,9 +1,11 @@
 import '@testing-library/dom';
 import '@testing-library/jest-dom';
-import { fetchSinToken } from '../../helpers/fetch';
+import { fetchConToken, fetchSinToken } from '../../helpers/fetch';
 
 
 describe('Pruebas en helpers/fetch.js', () => {
+
+    let token = '';
 
     test('fetch sin token debe de funcionar ', async() => {
         
@@ -12,7 +14,18 @@ describe('Pruebas en helpers/fetch.js', () => {
 
         const body = await resp.json();
         expect( body.ok ).toBe( true );
+
+        token = body.token;
     });
     
+    test('fetch con token debe de funcionar ', async() => {
+        
+        localStorage.setItem('token', token);
+
+        const resp = await fetchConToken( 'events/5f8c382524827f31fcdb1eb5', {}, 'DELETE' );
+        const body = await resp.json();
+        
+        expect( body.msg ).toBe('No existe evento por ese id');
+    });
 
 })
