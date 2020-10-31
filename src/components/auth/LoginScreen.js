5cd21cf7dedfad4468877192add44aef8d7b00b4
Manuel Blanco <manuel.blanco77@gmail.com>
2020-10-31 14:31:51 -0300

    Pruebas en <LoginScreen/> y modificacion de LoginScreen para que no tenga completos los campos del input por defecto

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -12,17 +12,17 @@ export const LoginScreen = () => {
     const dispatch = useDispatch();
 
     const [ formLoginValues, handleLoginInputChange ] = useForm({
-        lEmail: 'manuel@gmail.com',
-        lPassword: '123456'
+        lEmail: '',
+        lPassword: ''
     });
 
     const { lEmail, lPassword } = formLoginValues
     
     const [ formRegisterValues, handleRegisterInputChange ] = useForm({
-        rName: 'Manuel',
-        rEmail: 'manuel7@gmail.com',
-        rPassword: '123456',
-        rPassword2: '123456'
+        rName: '',
+        rEmail: '',
+        rPassword: '',
+        rPassword2: ''
     });
 
     const { rName, rEmail, rPassword, rPassword2 } = formRegisterValues;
